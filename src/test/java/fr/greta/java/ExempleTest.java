cd0420a4562aff09e9f6a55c173a93d237868a36
julien <julien>
2021-02-19 11:41:05 +0100

    [first]

---
+++ src/test/java/fr/greta/java/ExempleTest.java
@@ -0,0 +1,14 @@
+package fr.greta.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExempleTest {
+
+    @Test
+    public void exemple1() {
+        assertTrue(true);
+    }
+
+}
